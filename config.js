6ab6d6bd8d0cb9942d28161c16fc6048c413a511
Nextroflash <96528654+Nextroflash@users.noreply.github.com>
2024-08-24 15:11:37 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  webhookURL: 'https://discord.com/api/webhooks/1276611351967698975/pLfT57PP5PBqSc3LJF0te-dKFHBP754bJhUEWiGmOa5XrV8ze2STOHBw777bb4FEuB2j',
+  token: 'MTEzOTYzODQ2MjMyMTAxNjk0Mg.GMYJCx.vHsrYbs7QFkz12GI0WNildxpkjmCzM7ZhUh6nw',
+  channelId: '1276611319340073011',
+  messageId: ''  // Optional: If you want to edit an existing message, put its ID here; otherwise, leave it empty or null.
+};
