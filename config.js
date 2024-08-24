0e84e116f036b69ae66b187276fc2ca822457dc8
Nextroflash <96528654+Nextroflash@users.noreply.github.com>
2024-08-24 15:37:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  webhookURL: 'https://discord.com/api/webhooks/1276611351967698975/pLfT57PP5PBqSc3LJF0te-dKFHBP754bJhUEWiGmOa5XrV8ze2STOHBw777bb4FEuB2j',
-  messageId: '1276896878935674952'  // Optional: If you want to edit an existing message, put its ID here; otherwise, leave it empty or null.
+  webhookURL: 'https://discord.com/api/webhooks/1276898118453039288/mcKLHRFIjiluTpU2YvBLQx-p9L2cn5cMZu_ZOxXYt0_sNTCpwqmh8q6fiAJP0BiPBmyh',
+  messageId: '1276898188271419393'  // Optional: If you want to edit an existing message, put its ID here; otherwise, leave it empty or null.
 };
