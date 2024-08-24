9e3bd46fe7387db8280b5af4a0eb3f5cfc31720b
Nextroflash <96528654+Nextroflash@users.noreply.github.com>
2024-08-24 15:28:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  webhookURL: 'https://discord.com/api/webhooks/1276611351967698975/pLfT57PP5PBqSc3LJF0te-dKFHBP754bJhUEWiGmOa5XrV8ze2STOHBw777bb4FEuB2j',
+  token: process.env.DISCORD_TOKEN,
   channelId: '1276611319340073011',
-  messageId: ''  // Optional: If you want to edit an existing message, put its ID here; otherwise, leave it empty or null.
+  messageId: '' // Optional
 };
